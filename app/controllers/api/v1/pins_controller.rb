a609b0e0b3911fc7788b3b6d33b3d721678a1d73
Eric Suescun <edsuescun@gmail.com>
2019-07-11 15:33:26 -0500

    Added security through headers and tested with hhtparty

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PinsController < ApplicationController
   before_action :restrict_access
 
   def index
-    render json: Pin.all.order('created_at DESC')
+    render json: Pin.all.order('created_at DESC'), status: :ok
   end
 
   def create
@@ -21,14 +21,16 @@ class Api::V1::PinsController < ApplicationController
     end
 
     def restrict_access
+      # render :json=> {:message=>"Email: #{request.headers["HTTP_X_USER_EMAIL"]}, token: #{request.headers["HTTP_X_USER_TOKEN"]}"}, :status=>:ok
+      # render :json=> {:message=>"Headers #{request.headers["SERVER_PORT"]}"}, :status=>:ok
       # user1 = User.find(1)
-      user1 = User.find_by_email(request.headers["X-User-Email"])
-      user2 = User.find_by_api_token(request.headers["X-Api-Token"])
+      user1 = User.find_by_email(request.headers["HTTP_X_USER_EMAIL"])
+      user2 = User.find_by_api_token(request.headers["HTTP_X_USER_TOKEN"])
       # user2 = User.find(1)
-      if  user1 == user2
-        render :json=> {:message=>"Good!"}, :status=>:ok
-      else
+      if  user1 != user2
         render :json=> {:message=>"Error with your credentials"}, :status=>401
       end
     end
 end
+
+# httparty -H x-user-email:edsuescun@gmail.com -H x-user-token:f40b4da587b01bcbc2372e0ba74b4caa http://localhost:3000/api/v1/pins
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
+  # include Response
+  # include ExceptionHandler
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  # called before every action on controllers
+  # before_action :authorize_request
+  # attr_reader :current_user
+
+  # private
+
+  # 	# Check for valid request token and return user
+  # 	def authorize_request
+  # 	  @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  # 	end
+  	
 end
 
 
