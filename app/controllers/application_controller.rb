d8c90cf897ae2a02c1cddab4efd900a4ae2409d4
Eric Suescun <edsuescun@gmail.com>
2019-07-12 15:25:48 -0500

    Disabled including Response and Exception Handler in App Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  include Response
-  include ExceptionHandler
+  # include Response
+  # include ExceptionHandler
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
